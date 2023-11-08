86ab4ce9388d2e4bd3a6e35c8cafb7cbb7b040e4
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-08 20:37:27 +0800

    Update Factory.test.js

--- test/Factory.test.js
+++ test/Factory.test.js
@@ -6,25 +6,24 @@ describe("Factory Contract", function () {
   let walletFactory;
   let deployer, wallet1, wallet2;
   describe("Deploying contracts", function () {
-    it("will Emit Event", async function () {
+    it("will create a new wallet upon construction", async function () {
       [deployer, wallet1, wallet2] = await ethers.getSigners();
       const WalletFactory = await ethers.getContractFactory("Factory");
-      walletFactory = await WalletFactory.deploy();
-      await walletFactory.waitForDeployment();
-      console.log(
-        `walletFactory deployed to ${await walletFactory.getAddress()}`
+      walletFactory = await WalletFactory.deploy(
+        [deployer.address, wallet1.address, wallet2.address],
+        2
       );
+      await walletFactory.waitForDeployment();
+      //   console.log(
+      //     `walletFactory deployed to ${await walletFactory.getAddress()}`
+      //   );
 
-      await expect(
-        walletFactory.createNewWallet(
-          [deployer.address, wallet1.address, wallet2.address],
-          2
-        )
-      )
-        .to.emit(walletFactory, "WalletCreated")
-        .withArgs(0);
+      //   console.log(await walletFactory.walletArray(0));
+      expect(await walletFactory.walletArray(0)).to.equal(
+        "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be"
+      );
     });
-    it("will Emit Event With Index Equal 2", async function () {
+    it("will Emit Event With Index Equal 1", async function () {
       await expect(
         walletFactory.createNewWallet(
           [deployer.address, wallet1.address, wallet2.address],
