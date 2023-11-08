6d13690f43047aa0d8c04f7ba151d5b310ada4d8
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-08 13:14:13 +0800

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -3,7 +3,7 @@ const { task } = require("hardhat/config");
 task("getAddress", "Retrieve address from factory deployed contract")
   .addPositionalParam("walletIndex")
   .setAction(async (taskArgs, hre) => {
-    console.log(taskArgs);
+    // console.log(taskArgs);
     const walletFactory = await hre.ethers.getContractAt(
       "Factory",
       "0x8464135c8F25Da09e49BC8782676a84730C318bC"
