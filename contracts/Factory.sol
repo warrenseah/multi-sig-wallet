928696f7cf0e5222044e35209f0215d783f38890
Warren Seah <6447221+warrenseah@users.noreply.github.com>
2023-11-08 20:20:41 +0800

    Update Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,14 +4,22 @@ pragma solidity ^0.8.17;
 import "./MultiSigWallet.sol";
 
 contract Factory {
+   event WalletCreated(uint indexed _index);
+   
    MultiSigWallet[] public walletArray;
 
-   function CreateNewWallet(address[] memory _addresses, uint _quorem) public {
+   function createNewWallet(address[] memory _addresses, uint _quorem) public {
      MultiSigWallet wallet = new MultiSigWallet(_addresses, _quorem);
+     uint _index = walletArray.length;
      walletArray.push(wallet);
+      emit WalletCreated(_index);
    }
 
-   function wfGetter(uint _index) public view returns (address[] memory) {
-    return MultiSigWallet(payable(address(walletArray[_index]))).getOwners();
+    function getWalletList() public view returns (address[] memory) {
+    address[] memory list = new address[](walletArray.length); 
+    for(uint i=0; i < walletArray.length; i++){ 
+        list[i] = payable(address(walletArray[i])); 
+     } 
+    return list;
    }
 }
